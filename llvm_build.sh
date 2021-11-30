077a894d95257b42e1ae3e9e4a5af86811c1b745
Ilmari Heikkinen <hei@heichen.hk>
2021-11-30 14:26:55 +0800

    parallel build

--- llvm_build.sh
+++ llvm_build.sh
@@ -7,16 +7,19 @@ mkdir -p lib && cd lib &&
 
 for f in ../../zstd/lib/**/*.c
 do
-	clang -DZSTD_MINIFY_LIB=1 -DZSTD_NO_INLINE=1 -c --target=wasm32 -nostdlib -I sysroot/include -I../../zstd/lib -o `basename $f`.o $f
+	clang -DZSTD_MINIFY_LIB=1 -DZSTD_NO_INLINE=1 -c --target=wasm32 -nostdlib -I sysroot/include -I../../zstd/lib -o `basename $f`.o $f &
 done
+wait
 
-llvm-ar -r libzstd_compress.a `echo ../../zstd/lib/{common,compress}/*.c | sed -e 's/\.c/.c.o/g' -e 's/[^ ]*\///g'`
-llvm-ar -r libzstd_decompress.a `echo ../../zstd/lib/{common,decompress}/*.c | sed -e 's/\.c/.c.o/g' -e 's/[^ ]*\///g'`
+llvm-ar -r libzstd_compress.a `echo ../../zstd/lib/{common,compress}/*.c | sed -e 's/\.c/.c.o/g' -e 's/[^ ]*\///g'` &
+llvm-ar -r libzstd_decompress.a `echo ../../zstd/lib/{common,decompress}/*.c | sed -e 's/\.c/.c.o/g' -e 's/[^ ]*\///g'` &
 llvm-ar -r libzstd.a *.c.o
+wait
 )
 
-for suffix in '' _min _compress _decompress
+for suffix in '' _compress _decompress
 do
+(
 	exported_functions=../exported_functions${suffix}.exp
 	clang --target=wasm32 -nostdlib -Wl,--no-entry \
 		-Wl,--export,malloc -Wl,--export,calloc -Wl,--export,free -Wl,--export,setHeapPtr, \
@@ -28,4 +31,6 @@ do
 		-o zstd${suffix}-raw.wasm &&
 	wasm-opt -Oz zstd${suffix}-raw.wasm -o zstd${suffix}.wasm &&
 	rm zstd${suffix}-raw.wasm
+) &
 done
+wait
